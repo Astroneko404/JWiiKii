3e511c734f7b6a76b9b4ef77a098f7c454c0d9df
O'mletto Lagrange <33112694+Astroneko404@users.noreply.github.com>
2019-02-28 20:20:01 -0500

    init

---
+++ test.py
@@ -0,0 +1,28 @@
+import Mykytea
+
+
+def showTags(t, outfile):
+    for word in t:
+        out = word.surface + "\t"
+        for t1 in word.tag:
+            for t2 in t1:
+                for t3 in t2:
+                    out = out + "/" + str(t3)
+                out += "\t"
+            out += "\t"
+        outfile.write(out+'\n')
+
+
+infile = open('data/in.txt', 'r', encoding='utf-8')
+outfile = open('data/out.txt', 'w+', encoding='utf-8')
+
+opt = "-model full_svm.mod"
+mk = Mykytea.Mykytea(opt)
+
+while True:
+    line = infile.readline()
+    if not line:
+        break
+    print(line)
+    tag = mk.getTags(line)
+    showTags(tag, outfile)
