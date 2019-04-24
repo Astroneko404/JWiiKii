9841f079f1a52e33231e672ec79a4af593d9affb
Sherry0409 <shili9749@gmail.com>
2019-04-23 21:54:42 -0400

    pp

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def search():
             content = returnOriginContent(result_list[i]).readDocument()
             output.append(content)
 
-        return render_template('results.html', que=output, queries=searchquery)
+        return render_template('results.html', que=output, queries=searchquery, display="inline")
 
     if request.method == 'POST':
         posttype = request.form.get('posttype')
@@ -51,7 +51,7 @@ def search():
             for i in range(0, 5):
                 output.append(returnOriginContent(result_list[i]).readDocument())
 
-            return render_template('results.html', que=output, queries=newquery)
+            return render_template('results.html', que=output, queries=newquery, display="inline")
 
         if posttype == 'page':
             output = []
@@ -70,10 +70,15 @@ def search():
             for i in range(lower_bound, upper_bound):
                 output.append(returnOriginContent(result_list[i]).readDocument())
 
+            if session['num'] == 5:
+                display = "none"
+            else:
+                display = "inline"
+
             # print(num)
             # print(session['num'])
 
-            return render_template('results.html', que=output, queries=session['query'])
+            return render_template('results.html', que=output, queries=session['query'], display=display)
 
 
 if __name__ == '__main__':
